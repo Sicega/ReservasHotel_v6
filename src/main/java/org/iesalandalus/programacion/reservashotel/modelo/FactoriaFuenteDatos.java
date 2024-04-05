565ce6aa9b2308cd0dcedfd301c236ef4713b6e6
You-loop-great <silvia.cg12@gmail.com>
2024-04-05 21:10:31 +0200

    Cambios en Factoria

--- src/main/java/org/iesalandalus/programacion/reservashotel/modelo/FactoriaFuenteDatos.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/modelo/FactoriaFuenteDatos.java
@@ -1,4 +1,22 @@
 package org.iesalandalus.programacion.reservashotel.modelo;
 
-public enum FactoriaFuenteDatos {
+import org.iesalandalus.programacion.reservashotel.modelo.negocio.IFuenteDatos;
+import org.iesalandalus.programacion.reservashotel.modelo.negocio.memoria.FuenteDatosMemoria;
+import org.iesalandalus.programacion.reservashotel.modelo.negocio.mongodb.FuenteDatosMongoDB;
+
+public enum FactoriaFuenteDatos  {
+
+    MONGODB{
+        public IFuenteDatos crear(){
+            return new FuenteDatosMongoDB();
+        }
+    },
+
+    MEMORIA {
+        public IFuenteDatos crear(){
+            return new FuenteDatosMemoria();
+        }
+    };
+
+
 }
--- src/main/java/org/iesalandalus/programacion/reservashotel/modelo/negocio/IFuenteDatos.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/modelo/negocio/IFuenteDatos.java
@@ -1,4 +1,8 @@
 package org.iesalandalus.programacion.reservashotel.modelo.negocio;
 
 public interface IFuenteDatos {
+
+    IHuespedes crearHuespedes();
+    IHabitaciones crearHabitaciones();
+    IReservas crearReservas();
 }
