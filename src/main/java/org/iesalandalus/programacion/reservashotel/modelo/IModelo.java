acf1ace31335c8f923ac54eddeae4ac19c49b70b
You-loop-great <silvia.cg12@gmail.com>
2024-04-13 13:51:31 +0200

    Cambios en IModelo

--- src/main/java/org/iesalandalus/programacion/reservashotel/modelo/IModelo.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/modelo/IModelo.java
@@ -5,24 +5,25 @@ import org.iesalandalus.programacion.reservashotel.modelo.dominio.Huesped;
 import org.iesalandalus.programacion.reservashotel.modelo.dominio.Reserva;
 import org.iesalandalus.programacion.reservashotel.modelo.dominio.TipoHabitacion;
 
+import javax.naming.OperationNotSupportedException;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface IModelo {
     void comenzar();
     void terminar();
-    void insertar(Huesped huesped);
+    void insertar(Huesped huesped) throws OperationNotSupportedException;
     Huesped buscar(Huesped huesped);
-    void borrar(Huesped huesped);
+    void borrar(Huesped huesped) throws OperationNotSupportedException;
     List<Huesped> getHuespedes();
-    void insertar(Habitacion habitacion);
+    void insertar(Habitacion habitacion) throws OperationNotSupportedException;
     Habitacion buscar(Habitacion habitacion);
-    void borrar(Habitacion habitacion);
+    void borrar(Habitacion habitacion) throws OperationNotSupportedException;
     List<Habitacion> getHabitaciones();
     List<Habitacion>getHabitaciones(TipoHabitacion tipoHabitacion);
-    void insertar(Reserva reserva);
+    void insertar(Reserva reserva) throws OperationNotSupportedException;
     Reserva buscar(Reserva reserva);
-    void borrar(Reserva reserva);
+    void borrar(Reserva reserva) throws OperationNotSupportedException;
     List<Reserva> getReservas();
     List<Reserva> getReservas(Huesped huesped);
     List<Reserva> getReservas(TipoHabitacion tipoHabitacion);
